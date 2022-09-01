06531032f38f10433b76ab292062efbb2749ba81
Alfred <kobalt888@hotmail.com>
2022-08-31 23:00:19 -0700

    Updated readme, the polling rate and sample amounts, and more

--- moddable/tracker/main.js
+++ moddable/tracker/main.js
@@ -4,6 +4,10 @@ import Timer from "timer";
 
 if (config.ssid === "REPLACE")
 	throw new Error("Wi-Fi SSID required");
+if (config.passowrd === "REPLACE")
+	throw new Error("Wi-Fi Password required");
+if (config.device_id === "REPLACE")
+	throw new Error("Device ID required");
 
 import WiFi from "wifi";
 
@@ -11,34 +15,43 @@ WiFi.mode = 1;
 
 let networks = {};
 let postBody = {};
-
+let scanCount = 0;
 function scan() {
 	WiFi.scan({hidden: true}, item => {
 		if (item) {
-			networks[item.ssid] = item.rssi;
-		} else {
-			postBody = {
-				"deviceid": 24,
-				"networks": {
-					"payload": networks
-				}
+			if(networks[item.ssid]){
+				networks[item.ssid] = Math.round((networks[item.ssid]*scanCount + item.rssi) / (scanCount + 1))
 			}
+			else if (item.ssid != ""){
+				networks[item.ssid] = item.rssi;
+			}
+		} else {
+			scanCount += 1;
+			if(scanCount > 20){
+				scanCount = 0;
+				postBody = {
+					"deviceid": parseInt(config.device_id),
+					"networks": {
+						"payload": networks
+					}
+				}
 
-			networks = {}
-			trace(`${JSON.stringify(postBody)}\n`)
-		
-			let request = new Request({	
-				host: "wifitrak.ngrok.io",
-				path: "/api/v1/location/submitdata",
-				method: "POST",
-				body: JSON.stringify(postBody),
-				response: String
-			});
-		
-			request.callback = () => {}
+				networks = {}
+				trace(`${JSON.stringify(postBody)}\n`)
 
-			postBody = {}
-			Timer.set(id => scan(), 5000);
+				let request = new Request({	
+					host: "wifitrak.ngrok.io",
+					path: "/api/v1/location/submitdata",
+					method: "POST",
+					body: JSON.stringify(postBody),
+					response: String
+				});
+
+				request.callback = () => {}
+
+				postBody = {}
+			}
+			Timer.set(id => scan(), 500);
 		}
 	});
 }
